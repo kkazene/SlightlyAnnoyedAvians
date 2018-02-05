7c099faa892270ef666c6b4c3bd4d1bc469827d5
Tuuli Sarantola <tuuli.sarantola@atalent.fi>
2018-02-05 15:17:10 +0200

    Some prefabs and scenes and functinality

---
+++ Assets/Scripts/BallScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallScript : MonoBehaviour {
+
+	public Rigidbody2D ball;
+	public float releaseTime = 0.15f;
+	private bool clickedOn = false;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (clickedOn) {
+			ball.position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		}
+	}
+
+	void OnMouseDown () {
+		//spring.enabled = false;
+		ball.isKinematic = true;
+		clickedOn = true;		
+	}
+
+	void OnMouseUp () {
+		//spring.enabled = true;
+		ball.isKinematic = false;
+		clickedOn = false;
+		StartCoroutine(Release());
+	}
+
+	IEnumerator Release () {
+		yield return new WaitForSeconds(releaseTime);
+
+		GetComponent<SpringJoint2D>().enabled = false;
+		this.enabled = false;
+	}
+}
---
+++ Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour {
+
+	public float health = 4f;
+
+	void OnCollisionEnter2D (Collision2D colInfo)
+	{
+		if (colInfo.relativeVelocity.magnitude > health) {
+			Die ();
+		}
+
+	}
+
+	void Die () {
+		Destroy(gameObject);
+	}
+}
